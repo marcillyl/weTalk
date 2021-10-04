58a91bfdc7ab9c4019e9f78e8a0f96242d443aea
marcillyl <marcillyluc@icloud.com>
2021-10-04 11:52:13 +0200

    build(backend): create server + app

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const helmet = require('helmet');
+const userRoutes = require('./routes/user');
+const messageRoutes = require('./routes/message');
+const commentRoutes = require('./routes/comment');
+
+const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://marcillyl:t5Wt7Vx6qERWx4RR@marcillyl.jcucj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+app.use(express.json());
+app.use(helmet());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+app.use(
+  '/assets/images',
+  express.static(path.join(__dirname, 'assets/images'))
+);
+app.use('/api/users', userRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/comments', commentRoutes);
+
+module.exports = app;
